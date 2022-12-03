b2cc9d26975bb465cebd48255ff86bfd4b83dfe3
atsushi-maru <ikeda.atsushi00@gmail.com>
2022-12-03 18:20:48 +0900

    implement section fade-in

--- js/script.js
+++ js/script.js
@@ -7,4 +7,50 @@ $( function () {
     autoplaySpeed: 5000,
     arrows: false,
   });
+
+  // メニューのホバー時に色を薄くする
+  $('.menu-item').on('mouseover', function () {
+    $(this).animate({
+      opacity: 0.5,
+    }, 100);
+  });
+  $('.menu-item').on('mouseout', function () {
+    $(this).animate({
+      opacity: 1.0,
+    }, 100);
+  });
+
+  // スクロールが100pxに達したらTOPボタン表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#back-btn').fadeIn();
+    } else {
+      $('#back-btn').fadeOut();
+    };
+  })
+
+  // セクションをフェードインさせる
+  $(window).scroll(function() {
+    const scrollAmount = $(window).scrollTop();
+    const windowHeight = $(window).height();
+    $('section').each(function() {
+      const position = $(this).offset().top;
+      if (scrollAmount > position - windowHeight + 100) {
+        $(this).addClass('fade-in');
+      }
+    });
+  });
+
+  $('.works-section img').click(function() {
+    const imgSrc = $(this).attr('src');
+    $('.big-img').attr('src', imgSrc);
+    $('.modal').fadeIn();
+    return false
+  });
+
+  $('.close-btn').click(function() {
+    $('.modal').fadeOut();
+    return false
+  });
+
 });
